ffb47b6388fc96236a9f163f5ce4ec247adddf34
Vilyamov Maxim <moscow.idol.com@mail.ru>
2024-01-18 08:41:59 +0300

    add css add description

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,3 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.CharField(max_length=100, default='pusto')
+
+    def __repr__(self):
+        return f'Item({self.name}, {self.brand}, {self.count})'
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Item
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
